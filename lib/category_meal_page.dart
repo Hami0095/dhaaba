b696dc32f109355c927f4de31bebc71578fb2281
Hami0095 <abdman0095@gmail.com>
2022-05-21 09:20:50 +0500

    meals added

--- lib/category_meal_page.dart
+++ lib/category_meal_page.dart
@@ -1,3 +1,4 @@
+import 'package:dhaaba/dummy_data.dart';
 import 'package:flutter/material.dart';
 
 class CategoryMealPage extends StatelessWidget {
@@ -13,11 +14,14 @@ class CategoryMealPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
-    final String? argTitle = routeArgs['title'];
-    final String? argId = routeArgs['id'];
+    final String? categoryTitle = routeArgs['title'];
+    final String? categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
-        title: Text(argTitle!),
+        title: Text(categoryTitle!),
       ),
       body: const Center(
         child: Text('The recipies of the meals'),
